fbdd956d358f2317fd28a4064360605ef342bfe5
Xiaohulala <lingronghu05@gmail.com>
2021-05-18 11:57:14 +0800

    environment setup

---
+++ Course-Registration/src/main/java/com/hu/configs/MainConfig.java
@@ -0,0 +1,23 @@
+package com.hu.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages="com.hu")
+public class MainConfig {
+	// define a bean for ViewRosolver
+	@Bean
+	public ViewResolver viewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setPrefix("/WEB-INF/view/");
+		viewResolver.setSuffix(".jsp");
+		return viewResolver;
+	}
+
+}
